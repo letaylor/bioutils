74545642c177cbffd0aaf64993a9ba26a10f2abc
Leland Taylor <letaylor@users.noreply.github.com>
2019-03-26 10:48:11 -0700

    all

--- conda/deploy_anaconda.sh
+++ conda/deploy_anaconda.sh
@@ -16,19 +16,19 @@
 set -e
 
 CONDA_OUT_FILE=${1-"ERROR"} # for conda build .
-echo "CONDA_OUT_FILE=${CONDA_OUT_FILE}"
+#echo "CONDA_OUT_FILE=${CONDA_OUT_FILE}"
 
+# By adding --all to anaconda call, automatically run the conda convert
 # echo "Converting conda package..."
-# conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/PACKAGENAME-*.tar.bz2 --output-dir conda-bld/
+# conda convert --platform all ${CONDA_OUT_FILE} --output-dir conda-bld/
 
-# /home/travis/miniconda/conda-bld/linux-64/r-bioutils-0.1.1-r351_0.tar.bz2
 echo "Deploying to Anaconda.org..."
 anaconda --token ${CONDA_UPLOAD_TOKEN} upload \
     ${CONDA_OUT_FILE} \
     --user ${CONDA_USER_NAME} \
     --label "main" \
-    --force
-    #--all
+    --force \
+    --all
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
