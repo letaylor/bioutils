b21a291930bf83405f71debbc874c442410cfd24
Leland Taylor <letaylor@users.noreply.github.com>
2019-03-26 10:19:58 -0700

    .

--- conda/deploy_anaconda.sh
+++ conda/deploy_anaconda.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+#===============================================================================
+#title        :deploy_anaconda.sh
+#description  :
+#author       :Leland Taylor
+#date         :26/03/2019
+#version      :0.1
+#usage        :sh deploy_anaconda.sh PKG_NAME
+#input        :PKG_NAME (required)
+#               - package name to deploy
+#output       :NULL
+#notes        :CONDA_USER_NAME is stored securely in on Travis CI.
+#              CONDA_UPLOAD_TOKEN is stored securely in on Travis CI.
+#              See README.md
+#===============================================================================
 set -e
 
-# NOTE: This script uses the CONDA_UPLOAD_TOKEN env var. Store this token
-# securely in on Travis CI. See README.md
+PKG_NAME=${1-"ERROR"} # for conda build .
 CONDA_USER_NAME="letaylor" # for anaconda upload
-PKG_NAME="bioutils" # for conda build .
 
 # echo "Converting conda package..."
 # conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/PACKAGENAME-*.tar.bz2 --output-dir conda-bld/
