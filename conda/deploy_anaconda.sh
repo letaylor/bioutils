741dad8d9d4d3a66ed4540873785897eed7aa0f2
Leland Taylor <letaylor@users.noreply.github.com>
2019-03-25 22:11:38 -0700

    .

--- conda/deploy_anaconda.sh
+++ conda/deploy_anaconda.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-# this script uses the ANACONDA_TOKEN env var.
+set -e
+
+# NOTE:
+# this script uses the CONDA_UPLOAD_TOKEN env var.
 # to create a token:
 # >>> anaconda login
 # >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
-set -e
 
-echo "Converting conda package..."
-conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/PACKAGENAME-*.tar.bz2 --output-dir conda-bld/
+USER="letaylor" # for anaconda upload
+PKG_NAME="bioutils" # for conda build .
+
+# echo "Converting conda package..."
+# conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/PACKAGENAME-*.tar.bz2 --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
-anaconda -t ${CONDA_UPLOAD_TOKEN} upload conda-bld/**/PACKAGENAME-*.tar.bz2
+anaconda --token ${CONDA_UPLOAD_TOKEN} upload \
+    ${HOME}/miniconda/conda-bld/**/${PKG_NAME}-*.tar.bz2 \
+    --user ${USER} \
+    --label "main" \
+    --force
+    #--all
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
